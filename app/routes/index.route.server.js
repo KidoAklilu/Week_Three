4e73fed3b69958916cd08ac03f85426be7a0f040
KidoAklilu <Kidoget321@gmail.com>
2022-09-26 16:30:40 -0400

    Controller and route completed

---
+++ app/routes/index.route.server.js
@@ -0,0 +1,11 @@
+// index route
+import { Router } from 'express'
+
+import { helloWorld } from '../controllers/index.controller.server.js'
+
+const router = Router()
+
+router.get('/', helloWorld)
+router.get('/hello', helloWorld)
+
+export default router
--- server.js
+++ server.js
@@ -1,34 +1,43 @@
 // import express and third partmodules
-import express from "express";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
-import session from "express-session";
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import session from 'express-session'
 
 //ES modules fic for __dirname
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+//import router
+
+import indexRouter from './app/routes/index.route.server.js'
 
 //instance app-server
-const app = express();
+const app = express()
 
-//custom middleware
-function helloWorld(req, res, next) {
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
-}
+//set up viewENgine
+app.set('views', path.join(__dirname, '/views'))
+app.set('view engine', 'ejs')
 
-function byeWorld(req, res, next) {
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Good bye World");
-}
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, '../public')))
+app.use(
+  session({
+    secret: 'My secret',
+    saveUninitialized: false,
+    resave: false,
+  })
+)
 
-//add middeleware to connect application
-app.use("/hello", helloWorld);
-app.use("/bye", byeWorld);
-app.use("/", helloWorld);
+//add routes
+
+app.use('/', indexRouter)
 
 //run app
-app.listen(3000);
+app.listen(3000)
 
-console.log("Server runnning at http://localhost:3000");
+console.log('Server runnning at http://localhost:3000')
