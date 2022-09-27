a9b7daf5fb375780ba47b19c7cc307bb6008443d
KidoAklilu <Kidoget321@gmail.com>
2022-09-27 00:09:38 -0400

    views and partials completed

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,11 +1,21 @@
 // index route
 import { Router } from 'express'
 
-import { helloWorld } from '../controllers/index.controller.server.js'
+import {
+  displayAboutPage,
+  displayContactPage,
+  displayHomepage,
+  displayProjectPage,
+  displayServicesPage,
+} from '../controllers/index.controller.server.js'
 
 const router = Router()
 
-router.get('/', helloWorld)
-router.get('/hello', helloWorld)
+router.get('/', displayHomepage)
+router.get('/home', displayHomepage)
+router.get('/about', displayAboutPage)
+router.get('/contact', displayContactPage)
+router.get('/services', displayServicesPage)
+router.get('/projects', displayProjectPage)
 
 export default router
